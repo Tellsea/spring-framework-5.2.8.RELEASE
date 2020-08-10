3782dde69bef0162f9374926a0fb815e22cc6d20
tellsea <45138439+seaHung@users.noreply.github.com>
2020-08-10 14:34:28 +0800

    spring5.2.8源码初步构建完成

---
+++ spring-tellsea/src/main/java/cn/tellsea/config/AppConfig.java
@@ -0,0 +1,12 @@
+package cn.tellsea.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Tellsea
+ * @date 2020-8-10
+ */
+@ComponentScan("cn.tellsea")
+public class AppConfig {
+}
---
+++ spring-tellsea/src/main/java/cn/tellsea/service/UserInfoService.java
@@ -0,0 +1,11 @@
+package cn.tellsea.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Tellsea
+ * @date 2020-8-10
+ */
+@Component
+public class UserInfoService {
+}
